efb43e28e10dc36430a6dec02a81803e2b5585ac
LetWeDo <hijiwho@gmail.com>
2025-07-01 18:32:32 +0900

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lwd-pwa/sw.js', { scope: '/lwd-pwa/' })})}
